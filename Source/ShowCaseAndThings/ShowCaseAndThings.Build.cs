315b9cd41fe271abd4fc7ff71b468ada6e6d9cd2
HideNknow <outin.matthieu@gmail.com>
2022-06-10 11:20:43 +0200

    Initial Push

---
+++ Source/ShowCaseAndThings/ShowCaseAndThings.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ShowCaseAndThings : ModuleRules
+{
+	public ShowCaseAndThings(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
---
+++ Source/ShowCaseAndThings/ShowCaseAndThingsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShowCaseAndThingsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AShowCaseAndThingsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AShowCaseAndThingsGameMode();
+};
+
+
+
